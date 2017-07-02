6ed0b0f198e71cbfa4fec5106e3a59116e97bc94
Sam Wu <sjwu@ualberta.ca>
2017-07-02 15:42:22 -0700

    school example

---
+++ ArrayList/src/school/School.java
@@ -0,0 +1,33 @@
+package school;
+
+import java.util.ArrayList;
+
+public class School {
+	public static void main(String[] args) {
+        //ArrayLists are dynamic collections of objects (which can be reference types only)
+        ArrayList<Student> compsci = new ArrayList<>();
+        
+        //Add students to Student ArrayList
+        compsci.add(new Student("Arnold", 3.0));
+        compsci.add(new Student("Bob", 3.3));
+        compsci.add(new Student("Cam", 4.0));
+        compsci.add(new Student("Dale", 3.7));
+        
+        //Access as a collection
+        System.out.println("Example using Enumeration");
+        System.out.printf("Student\tGPA\n");
+        //for each student in the compsci ArrayList
+        for(Student s : compsci) {
+            System.out.printf("%s\n", s);
+        }
+        System.out.println();
+        
+        //Access with an index, similar to an array
+        System.out.println("Example using Index");
+        System.out.printf("Student\tGPA\n");
+        for(int i = 0; i < compsci.size(); i++) {
+            System.out.printf("%s\n",compsci.get(i));
+        }
+                
+    }
+}
---
+++ ArrayList/src/school/Student.java
@@ -0,0 +1,34 @@
+package school;
+
+public class Student {
+	private String Name;
+    private double GPA;
+    
+    //constructor
+    public Student(String name, double gpa) {
+        this.Name = name;
+        this.GPA = gpa;
+    }
+    
+    public String getName() {
+        return this.Name;
+    }
+    
+    public String setName(String newName) {
+        return (this.Name = newName);
+    }
+    
+    public double getGPA() {
+        return this.GPA;
+    }
+    
+    public double setGPA(double newGPA) {
+        return (this.GPA = newGPA);
+    }
+    
+    @Override
+    //print student name and gpa as string
+    public String toString() {
+        return String.format("%s\t%f", this.Name, this.GPA);
+    }
+}
