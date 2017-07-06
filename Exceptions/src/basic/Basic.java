5fbccd1916e3d8ee007e3d668e589f72f52e5c11
Sam Wu <sjwu@ualberta.ca>
2017-07-05 22:18:10 -0700

    add dowhile loop;

--- Exceptions/src/basic/Basic.java
+++ Exceptions/src/basic/Basic.java
@@ -3,25 +3,29 @@ package basic;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+//This will throw an InputMismatchException if a non-integer is entered
 public class Basic {
 	public static void main(String[] args) {
-	       //This will throw an InputMismatchException a non-integer is entered
-	       @SuppressWarnings("resource") //for unclosed Scanner resource input
-	       Scanner input = new Scanner(System.in);
-	       System.out.print("Enter an integer : ");
-	       
-	       //code to try
-	       try {
-			int i = input.nextInt();
-	        System.out.printf("You entered the integer %d\n", i);
-	       }
-	       //exception handling - occurs if try has an error
-	       catch (InputMismatchException exception) {
-	           System.out.println("Exception. You didn't enter an integer.");
-	       }
-	       //code that always runs (regardless of whether try or catch ran)
-	       finally {
-	           System.out.println("Finally happens whether the exception occurs or not");
-	       }
+	       boolean gotinput = false;
+
+	       //run the code at least once
+	       do {
+	    	   //code to try
+	    	   try {
+	    		   Scanner input = new Scanner(System.in);
+	    		   System.out.print("Enter an integer : ");
+	    		   int i = input.nextInt();
+	    		   System.out.printf("You entered the integer %d\n", i);
+	    		   gotinput = true; //will not occur if exception is thrown
+	    	   }
+	    	   //exception handling - occurs if try has an error
+	    	   catch (InputMismatchException exception) {
+	    		   System.out.println("Exception. You didn't enter an integer.");
+	    	   }
+	    	   //code that always runs (regardless of whether try or catch ran)
+	    	   finally {
+	    		   System.out.println("Finally code happens whether the exception occurs or not");
+	    	   }
+	       } while (!gotinput); //continue running code until get valid input
 	    }
 }
