78818004223bc3acb8607770c0fd983381530945
Sam Wu <sjwu@ualberta.ca>
2017-07-05 22:29:22 -0700

    add loop

--- Exceptions/src/inputexception/IntegerRange.java
+++ Exceptions/src/inputexception/IntegerRange.java
@@ -7,20 +7,26 @@ import java.util.Scanner;
 public class IntegerRange {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-	       Scanner input = new Scanner(System.in);
-	       int i;
-	       System.out.print("Enter an integer between 1 and 10: ");
-	       try {
-	           i = input.nextInt();
-	           if (i < 1 || i > 10) {throw new RangeException();}
-	       }
-	       //for invalid data type input
-	       catch (InputMismatchException ex) {
-	           System.out.println("You did not enter an integer.");
-	       }
-	       //for invalid data range input
-	       catch (RangeException ex) {
-	           System.out.println("Your input value is not in the specified range");
-	       }  
-	    }
+		boolean validinput = false;
+	    int i;	    
+	       
+	    do {
+	    	try {
+	    		Scanner input = new Scanner(System.in); //must be in scope of try
+	    		System.out.print("Enter an integer between 1 and 10: ");
+	    		i = input.nextInt();
+	    		if (i < 1 || i > 10) {throw new RangeException();}
+	    		validinput = true;
+	    		System.out.printf("You entered the integer %d\n", i);
+	    	}
+	    	//for invalid data type input
+	    	catch (InputMismatchException ex) {
+	    		System.out.println("You did not enter an integer.");
+	    	}
+	    	//for invalid data range input
+	    	catch (RangeException ex) {
+	    		System.out.println("Your input value is not in the specified range");
+	    	}
+	    } while (!validinput);
+	}
 }
