87e82f99b9f75b14771237cfa80399e65a89bd5e
Sam Wu <sjwu@ualberta.ca>
2017-07-06 21:49:34 -0700

    serializing obj

--- FileInput&Output/src/objectserialization/SerializeObject.java
+++ FileInput&Output/src/objectserialization/SerializeObject.java
@@ -5,11 +5,44 @@ import java.util.ArrayList;
 
 /**
  * 
+ * Serialize: object to bytes
+ * Deserialize: bytes to object
  * FileInputStream/FileOutputStream used for reading/writing bytes to files
- * ObjectInputStream/ObjectOutputStream used for serializing/deserializing data into object
+ * ObjectInputStream/ObjectOutputStream used for deserializing/serializing data into object
  *
  */
-@SuppressWarnings("unused")
 public class SerializeObject {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+        File file = new File("students.txt");
+        
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(new Student("Tom", 3.9));
+        students.add(new Student("Dave", 4.0));
+        students.add(new Student("Bill", 3.0));
+        
+        FileOutputStream fo = new FileOutputStream(file); //write bytes to text file
+        ObjectOutputStream output = new ObjectOutputStream(fo); //serialize data for writing to text file (disk)
+        for (Student s : students) {
+            output.writeObject(s);
+        }
+        output.close();
+        fo.close();
+        
+        FileInputStream is = new FileInputStream(file);
+        ObjectInputStream input = new ObjectInputStream(is);
 
+        try {
+            while(true) {
+                    Student s = (Student)input.readObject();
+                    System.out.println(s);
+            }
+        } catch (EOFException ex) {
+            
+        }
+        
+        
+        
+                
+    }
 }
