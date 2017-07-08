a5d3b514ee58ccaf7b3824a5e5fe45f3c88d34ca
Sam Wu <sjwu@ualberta.ca>
2017-07-07 21:06:07 -0700

    creating jframe by association methods

---
+++ JavaSwingGUI/src/frames/AssociationConstructor.java
@@ -0,0 +1,26 @@
+package frames;
+
+import javax.swing.*;
+
+//creating the JFrame obj in the java constructor
+public class AssociationConstructor {  
+	JFrame f;
+	
+	//define AssociationConstructor object
+	AssociationConstructor(){  
+		f = new JFrame(); //creating instance of JFrame  
+          
+		JButton b = new JButton("click"); //creating instance of JButton  
+		b.setBounds(130, 100, 100, 40);  
+          
+		f.add(b); //adding button in JFrame  
+          
+		f.setSize(400,500); //400 width, 500 height  
+		f.setLayout(null); //no layout managers  
+		f.setVisible(true); //make the frame visible  
+	}  
+  
+	public static void main(String[] args) {  
+		new AssociationConstructor();
+	}  
+}  
--- JavaSwingGUI/src/frames/AssociationMain.java
+++ JavaSwingGUI/src/frames/AssociationMain.java
@@ -1,18 +1,19 @@
-package swingcontrols;
+package frames;
 
 import javax.swing.*;
 
-public class SwingJButton {
+//creating the JFrame obj in the main method
+public class AssociationMain {
 	public static void main(String[] args) {
 		JFrame javaframe = new JFrame();//create an instance of JFrame  
     
 		JButton javabutton = new JButton("Click Here"); //create an instance of JButton  
-		javabutton.setBounds(130, 100, 100, 40);//x axis, y axis, width, height  
+		javabutton.setBounds(130, 100, 100, 40); //x axis, y axis, width, height  
 	          
 		javaframe.add(javabutton); //add button to JFrame  
 	          
 		javaframe.setSize(400,500); //400 width, 500 height  
 		javaframe.setLayout(null); //no layout managers  
 		javaframe.setVisible(true); //make frame visible  
-}
+	}
 }
