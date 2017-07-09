b63ffb59588d22090d90b51bd8eedce74ea23dcf
Sam Wu <sjwu@ualberta.ca>
2017-07-08 22:26:15 -0700

    jpassworldfield

--- JavaSwingGUI/src/jframe/AssociationMain.java
+++ JavaSwingGUI/src/jframe/AssociationMain.java
@@ -1,4 +1,4 @@
-package frames;
+package jframe;
 
 import javax.swing.*;
 
---
+++ JavaSwingGUI/src/jpasswordfield/EnterPassword.java
@@ -0,0 +1,21 @@
+package jpasswordfield;
+
+import javax.swing.*;
+
+public class EnterPassword {
+	public static void main(String[] args) {
+    JFrame f=new JFrame("Password Field");
+    
+    JPasswordField value = new JPasswordField();
+    value.setBounds(100, 100, 200, 30); 
+    
+    JLabel l1 = new JLabel("Password:");
+    l1.setBounds(20, 100, 80, 30);
+    
+    f.add(value);
+    f.add(l1);
+    f.setSize(400, 300);
+    f.setLayout(null);
+    f.setVisible(true);
+}
+}
