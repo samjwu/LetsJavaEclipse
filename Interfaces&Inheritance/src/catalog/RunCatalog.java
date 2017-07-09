6de6f881171b0908bd6209796045013fbd117dd3
Sam Wu <sjwu@ualberta.ca>
2017-07-08 22:21:13 -0700

    formatting

--- Interfaces&Inheritance/src/catalog/RunCatalog.java
+++ Interfaces&Inheritance/src/catalog/RunCatalog.java
@@ -11,9 +11,9 @@ public class RunCatalog {
         products.add(new CD("Billie Jean", 19.99, "MJ", 5));
         products.add(new Product("Paper", 0.99));
         
-        System.out.printf("%-10s\t%10s\t%-10s\n", "NAME", "PRICE", "TYPE");
+        System.out.printf("%s %20s %20s \n", "NAME", "PRICE", "TYPE");
         for (Product p : products) {
-            System.out.printf("%-10s\t%8.2f\t%-10s\n", p.getName(), p.getPrice(), p.getType());
+            System.out.printf("%11s %13.2f %20s \n", p.getName(), p.getPrice(), p.getType());
         }
     }
     
