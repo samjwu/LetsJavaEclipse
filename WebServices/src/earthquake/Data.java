17a54ac81b603ac6382f7ccc483be00a7b0d2bbf
Sam Wu <sjwu@ualberta.ca>
2017-07-09 20:52:51 -0700

    Data class

--- WebServices/src/earthquake/Data.java
+++ WebServices/src/earthquake/Data.java
@@ -1,5 +1,41 @@
 package earthquake;
 
-public class Data {
+import java.util.Date;
 
+public class Data {
+	//earthquake information
+    private Date date;
+    private String details;
+    private String location;
+    private double magnitude;
+    private String link;
+    
+    //constructor
+    public Data(Date date, String details, String location, double magnitude, String link) {
+    	this.date =  date;
+        this.details = details;
+        this.location = location;
+        this.magnitude = magnitude;
+        this.link = link;
+    }
+
+    public Date getDate() {   
+        return this.date;    
+    }
+    
+    public String getDetails() {   
+        return details;   
+    }
+    
+    public String getLocation() {
+        return location;   
+    }
+    
+    public double getMagnitude()  {   
+        return magnitude;   
+    }
+    
+    public String getLink() {
+        return link;   
+    }
 }
